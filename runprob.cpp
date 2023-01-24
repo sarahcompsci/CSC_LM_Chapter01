27ca05b736b179a44ddd62f116b4c72f638e06fc
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-01-24 11:48:18 -0500

    Create runprob.cpp

---
+++ runprob.cpp
@@ -0,0 +1,24 @@
+// This program will take a number and divide it by 2.
+// Sarah Bender
+#include <iostream>
+using namespace std;
+int main() {
+float number;
+int divider;
+divider = 2;
+cout << "Hi there" << endl;
+cout << "Please input a number and then hit return" << endl;
+cin >> number;
+number = number / divider;
+cout << "Half of your number is " << number << endl;
+return 0;
+}
+
+//Re-compile and run the program. Type 9 when asked for input. Record what is printed.
+// Half of your number is 4.5
+
+//Run the program using different values. Record the output.
+//I ran the program using the value 10. The output was "Half of your number is 5."
+
+//Do you feel that you are getting valid output?
+//Yes.
