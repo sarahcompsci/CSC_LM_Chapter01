881e84097db32670d6791ae51cd16941693f9913
sarahcompsci <123202075+sarahcompsci@users.noreply.github.com>
2023-01-24 11:29:32 -0500

    Create firstprog.cpp

---
+++ firstprog.cpp
@@ -0,0 +1,16 @@
+//This is the first program that just writes out a simple message
+//Sarah Bender
+
+#include <iostream> //needed to perform C++ I/O
+using namespace std;
+
+int main() {
+  cout << "Now is the time for all good men" << endl;
+  cout << "To come to the aid of their party" << endl;
+
+  return 0;
+}
+
+//Run the program and write what is printed on the screen.
+//Now it is the time for all good men
+//To come to the aid of their party
